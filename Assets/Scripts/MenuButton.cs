e4858759f006d2757add539d60a122168307a248
JackLaFreniere <97800485+JackLaFreniere@users.noreply.github.com>
2025-06-06 22:02:03 -0500

    Scene Creation
    The Menu Scene and the Game Scene were created. Basic functionality was added to allow the Menu Scene to exit the application and go to the Game Scene. The Game Scene was able to go to the Menu Scene by hitting the Escape Key

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MenuButton : MonoBehaviour
+{
+    public void LoadGameScene(Object obj)
+    {
+        SceneManager.LoadScene("GameScene");
+        Destroy(obj);
+    }
+    public void ExitMenuScene()
+    {
+        if (Application.isEditor)
+        {
+            EditorApplication.ExitPlaymode();
+        } 
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
