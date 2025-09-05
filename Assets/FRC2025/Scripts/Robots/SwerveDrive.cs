c03c8a605d4d01acba5cdd50a687c647f1224019
JackLaFreniere <97800485+JackLaFreniere@users.noreply.github.com>
2025-09-05 07:32:37 -0500

    Create robot 9999 prefab blank drivetrain scripts
    Created a prefab for the test robot number 9999, and renamed the robotlibrary and robot pose to 9999 instead of 930. Implemented an empty tank and swerve drive script.

---
+++ Assets/FRC2025/Scripts/Robots/SwerveDrive.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace FRC2025
+{
+    public class SwerveDrive : Subsystem
+    {
+        public override string SubsystemName => throw new System.NotImplementedException();
+
+        public override void ApplyTarget(float value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsAtTarget()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
